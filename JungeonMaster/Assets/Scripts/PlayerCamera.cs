021fa00f667526a065d9b5e47647d9b70ed2aa01
Iksburg <goryainov.dm@edu.spbstu.ru>
2022-07-13 18:10:55 +0300

    Fixed camera rotation

--- JungeonMaster/Assets/Scripts/PlayerCamera.cs
+++ JungeonMaster/Assets/Scripts/PlayerCamera.cs
@@ -1,11 +1,10 @@
-using Unity.Mathematics;
 using UnityEngine;
 
 public class PlayerCamera : MonoBehaviour
 {
-
     public float sensX;
     public float sensY;
+    
     public Transform orientation;
 
     private float _xRotation;
@@ -18,16 +17,17 @@ public class PlayerCamera : MonoBehaviour
 
     void Update()
     {
+        // get mouse input
         float mouseX = Input.GetAxisRaw("Mouse X") * Time.deltaTime * sensX;
         float mouseY = Input.GetAxisRaw("Mouse Y") * Time.deltaTime * sensY;
 
         _yRotation += mouseX;
-        _yRotation = Mathf.Clamp(_yRotation, -90f, 90f);
-        
+
         _xRotation -= mouseY;
         _xRotation = Mathf.Clamp(_xRotation, -90f, 90f);
         
-        transform.rotation = quaternion.Euler(_xRotation, _yRotation, 0);
+        // rotate cam and orientation
+        transform.rotation = Quaternion.Euler(_xRotation, _yRotation, 0);
         orientation.rotation = Quaternion.Euler(0, _yRotation, 0);
     }
 }
