eec5ef8c537766b02233fe5164a270e142edc2ba
Iksburg <goryainov_mitya@mail.ru>
2022-08-11 01:42:08 +0300

    Added colourmap and meshmap

--- JungeonMaster/Assets/Scripts/Procedural Generation/MapGenerator.cs
+++ JungeonMaster/Assets/Scripts/Procedural Generation/MapGenerator.cs
@@ -5,6 +5,15 @@ namespace Procedural_Generation
 {
     public class MapGenerator : MonoBehaviour
     {
+        public enum DrawMode
+        {
+            NoiseMap, 
+            ColourMap,
+            Mesh
+        }
+
+        public DrawMode drawMode;
+        
         public int mapWidth;
         public int mapHeight;
         public float noiseScale;
@@ -19,12 +28,42 @@ namespace Procedural_Generation
 
         public bool autoUpdate;
 
+        public TerrainType[] regions;
+
         public void GenerateMap()
         {
             float[,] noiseMap = Noise.GenerateNoiseMap(mapWidth, mapHeight, seed, noiseScale, octaves, persistance, lacunarity, offset);
-
+            Color[] colourMap = new Color[mapWidth * mapHeight];
+            
+            for (int y = 0; y < mapHeight; y++)
+            {
+                for (int x = 0; x < mapWidth; x++)
+                {
+                    float currentHeight = noiseMap[x, y];
+                    for (int i = 0; i < regions.Length; i++)
+                    {
+                        if (currentHeight <= regions[i].height)
+                        {
+                            colourMap[y * mapWidth + x] = regions[i].colour;
+                            break;
+                        }
+                    }
+                }
+            }
+            
             MapDisplay display = FindObjectOfType<MapDisplay>();
-            display.DrawNoiseMap(noiseMap);
+            if (drawMode == DrawMode.NoiseMap)
+            {
+                display.DrawTexture(TextureGenerator.TextureFromHeightMap(noiseMap));
+            }
+            else if (drawMode == DrawMode.ColourMap)
+            {
+                display.DrawTexture(TextureGenerator.TextureFromColourMap(colourMap, mapWidth, mapHeight));
+            }
+            else if (drawMode == DrawMode.Mesh)
+            {
+                display.DrawMesh(MeshGenerator.GenerateTerrainMesh(noiseMap), TextureGenerator.TextureFromColourMap(colourMap, mapWidth, mapHeight));
+            }
         }
 
         private void OnValidate()
@@ -41,5 +80,13 @@ namespace Procedural_Generation
             if (octaves < 0)
                 octaves = 0;
         }
+    }
+    
+    [System.Serializable]
+    public struct TerrainType
+    {
+        public string name;
+        public float height;
+        public Color colour;
     }
 }
--- JungeonMaster/Assets/Scripts/Procedural Generation/Noise.cs
+++ JungeonMaster/Assets/Scripts/Procedural Generation/Noise.cs
@@ -36,8 +36,8 @@ namespace Procedural_Generation
                     
                     for (int i = 0; i < octaves; i++)
                     {
-                        float sampleX = (x - halfWidth) / scale * frequency + octaveOffsets[i].x;
-                        float sampleY = (y - halfHeight) / scale * frequency + octaveOffsets[i].y;
+                        float sampleX = (x - halfWidth) / scale * frequency + octaveOffsets[i].x * frequency;
+                        float sampleY = (y - halfHeight) / scale * frequency - octaveOffsets[i].y * frequency;
 
                         float perlinValue = Mathf.PerlinNoise(sampleX, sampleY) * 2 - 1;
                         noiseHeight += perlinValue * amplitude;
